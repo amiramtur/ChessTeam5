f8c32ec6fa1756e0643081cd71becbcda2b43017
amiramtur <amiramtur22@gmail.com>
2024-12-21 21:39:34 +0200

    Feat: Implemented class 'Board' (including header and cpp file).

--- Chess/Board.cpp
+++ Chess/Board.cpp
@@ -1,7 +1,36 @@
-#include "Board.h"
+#pragma once
 
-board::board()
+#include "Board.h"
+#include "Player.h"
+
+#define PieceString "RNBQKBNRPPPPPPPP################################pppppppprnbkqbnr"
+
+
+// constructor 
+Board::Board()
 {
-	this->_theBoard = "RNBQKBNRPPPPPPPP################################pppppppprnbkqbnr";
-	this->_turn = 0; 
+	this->_board = PieceString;
+	this->_player1 = Player(0); // doesn't need fixing, add a parameter in Player's constructor
+	this->_player2 = Player(1); // doesn't need fixing, add a parameter in Player's constructor
+	this->_turn = 1;
+}
+
+// destructor
+Board::~Board()
+{
+	this->_board = "";
+	~Player(this->_player1); // doesn't need fixing, add a Player destructor
+	~Player(this->_player2); // doesn't need fixing, add a Player destructor
+}
+
+// getter
+int Board::get_turn()
+{
+	return this->_turn;
+}
+
+// other
+std::string Board::update_board(std::string board)
+{
+	this->_board = board;
 }
--- Chess/Board.h
+++ Chess/Board.h
@@ -1,12 +1,24 @@
 #pragma once
 #include <string>
+#include "Player.h"
 
-class board
+class Board
 {
 private:
-	std::string _theBoard; //big letter - white, small letter - black 
-	int _turn; //0 - white 1 - black 
+	std::string _board; // BIG LETTER - WHITE | small letter - black 
+	int _turn; // white - 0 | black - 1
+	Player _player1;
+	Player _player2;
 public:
-	//constructor 
-	board(); 
+	// constructor 
+	Board();
+
+	// destructor
+	~Board();
+
+	// getter
+	int get_turn();
+
+	// other
+	std::string update_board(std::string board);
 };
