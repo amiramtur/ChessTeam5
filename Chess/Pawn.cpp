575baf6b6dfb93356f929c3e7c0d5054e3001a5f
amiramtur <amiramtur22@gmail.com>
2025-01-04 19:00:41 +0200

    Build: Reworked Pawn's 'move' method.

--- Chess/Pawn.cpp
+++ Chess/Pawn.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib> // for "abs()"
 
 #define WHITE 0
+#define BLACK 1
 
 Pawn::Pawn(const int color, Point* sp) : Piece()
 {
@@ -38,19 +39,63 @@ bool Pawn::move(const std::vector<Piece*>& board, const Point& dstp) const
 {
 	char sPiece = Point::get_piece(*this->_srcp, board), dPiece = Point::get_piece(dstp, board);
 	int dx = dstp.get_x() - this->_srcp->get_x(), dy = dstp.get_y() - this->_srcp->get_y(); // d = distance 
-	bool start = this->_srcp->get_y() == 2; 
+	bool start = std::abs(this->_srcp->get_y()) == 2; 
 	bool isOk = true;
 
 	if (Piece::first_check(board, *this->_srcp, dstp, this->_color))
 	{ 
 		isOk = false;
 	}
-	else if (this->_color == 1 && ((start == true && dy < 2) || (start == false && dy < 1 ) ||
-			(std::abs(dx) > 1 && dPiece == '#') || dy > 0 || (dx == 0 && dPiece != '#')))  // pawn check
-	{
+
+
+
+	// Pawn check
+	int direction = (this->_color == WHITE) ? 1 : -1;
+	int maxDy = start ? 2 : 1;
+
+	// normal movement
+	if (dy * direction < 1 || dy * direction > maxDy || std::abs(dx) > 1) {
+		isOk = false;
+		throw 6; // code 6
+	}
+	// eating
+	if (std::abs(dx) == 1 && dPiece == '#') {
 		isOk = false;
 		throw 6; // code 6
 	}
 	
 	return isOk; 
 }
+
+
+// pawn check
+	//if ((start && this->_color == WHITE) && 
+	//	((dy > 2) || (dy < 1) // normal movement
+	//	|| (std::abs(dx) > 1) || (std::abs(dx) == 1 && Point::get_piece(dstp, board) == '#'))) // eating
+	//{
+	//	isOk = false;
+	//	throw 6; // code 6
+	//}
+
+	//else if ((start && this->_color == BLACK) &&
+	//		((dy < 2) || (dy > 1) // normal movement
+	//		|| (std::abs(dx) > 1) || (std::abs(dx) == 1 && Point::get_piece(dstp, board) == '#'))) // eating
+	//{
+	//	isOk = false;
+	//	throw 6; // code 6
+	//}
+
+	//else if ((!start && this->_color == WHITE) &&
+	//		((dy > 1) || (dy < 1) // normal movement
+	//		|| (std::abs(dx) > 1) || (std::abs(dx) == 1 && Point::get_piece(dstp, board) == '#'))) // eating
+	//{
+	//	isOk = false;
+	//	throw 6; // code 6
+	//}
+	//else if ((start && this->_color == BLACK) &&
+	//		((dy < 1) || (dy > 1) // normal movement
+	//		|| (std::abs(dx) > 1) || (std::abs(dx) == 1 && Point::get_piece(dstp, board) == '#'))) // eating
+	//{
+	//	isOk = false;
+	//	throw 6; // code 6
+	//}
